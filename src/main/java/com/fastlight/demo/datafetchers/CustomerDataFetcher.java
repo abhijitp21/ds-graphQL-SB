5ee16a3be0f51c4d7a259d0127c3bfb06d0032c3
Abhijit Pachegaokar <abhijitp21@gmail.com>
2025-01-13 12:26:51 -0500

    Update CustomerDataFetcher.java

--- src/main/java/com/fastlight/demo/datafetchers/CustomerDataFetcher.java
+++ src/main/java/com/fastlight/demo/datafetchers/CustomerDataFetcher.java
@@ -44,6 +44,7 @@ public class CustomerDataFetcher {
     @DgsData(parentType = "Customer", field = "accounts")
     public List<Account> getCustomerAccounts(DgsDataFetchingEnvironment environment) {
         Customer customer = environment.getSource();
+        customer = null;
         return customer.getAccounts().stream()
                 .map(account -> fetchAccountById(account.getAccountId()))
                 .collect(Collectors.toList());
